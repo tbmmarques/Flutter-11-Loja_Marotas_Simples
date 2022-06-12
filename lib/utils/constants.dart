5e32530941f240da3d8868a6f1e910fb3aeb5698
thiago.marques <thiago.marques@e-construmarket.com.br>
2022-06-11 22:22:34 -0300

    Finalizando APP: Requisicoes HTTP

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const PRODUCT_BASE_URL =
+      'https://lojacod3r-default-rtdb.firebaseio.com/products';
+  static const ORDER_BASE_URL =
+      'https://lojacod3r-default-rtdb.firebaseio.com/orders';
+}
